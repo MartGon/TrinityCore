1a1a0771c47b117b46013a67efc68cf889e3eefd
MartGon <gudo35@gmail.com>
2017-06-18 00:13:52 +0200

    Fixed SQL issues

--- sql/updates/world/master/2017_99_99_99_world.sql
+++ sql/updates/world/master/2017_99_99_99_world.sql
@@ -21,12 +21,12 @@ DELETE FROM `areatrigger_template` WHERE `Id`=3157;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3157, 0, 0, 5, 5, 0, 0, 0, 0, "areatrigger_hun_caltrops", 23911);
 
-DELETE FROM `spell_areatrigger` WHERE `SpellMiscId`=3157;
+DELETE FROM `spell_areatrigger` WHERE `AreaTriggerId`=3157;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
 (5084, 3157, 0, 0, 0, 0, 0, 0, 15000, 23911);
 
 -- 199483 Camouflage
-DELETE FROM `spell_script_names` WHERE `entry`=199483;
+DELETE FROM `spell_script_names` WHERE `spell_id`=199483;
 INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(199483,"spell_hun_camouflage");
 
 -- 1543 Flare areatrigger_template and areatrigger script
@@ -34,7 +34,7 @@ DELETE FROM `areatrigger_template` WHERE Id=3154;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3154, 0, 0, 10, 10, 0, 0, 0, 0, "areatrigger_hun_flare", 23911);
 
-DELETE FROM `spell_areatrigger` WHERE SpellMiscId=3154
+DELETE FROM `spell_areatrigger` WHERE `AreaTriggerId`=3154;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
 (510, 3154, 0, 0, 0, 0, 0, 0, 2000, 23911);
 
@@ -43,7 +43,7 @@ DELETE FROM `areatrigger_template` WHERE `Id`=3158;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3158, 0, 0, 5, 5, 0, 0, 0, 0, "areatrigger_hun_explosive_trap", 23911);
 
-DELETE FROM `spell_areatrigger` WHERE `SpellMiscId`=3158;
+DELETE FROM `spell_areatrigger` WHERE `AreaTriggerId`=3158;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
 (1613, 3158, 0, 0, 0, 0, 0, 0, 60000, 23911);
 
@@ -56,16 +56,16 @@ DELETE FROM areatrigger_template WHERE Id=3155;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3155, 0, 0, 3, 3, 0, 0, 0, 0, "areatrigger_hun_freezing_trap", 23911);
 
-DELETE FROM spell_areatrigger WHERE SpellMiscId=4424;
+DELETE FROM spell_areatrigger WHERE `AreaTriggerId`=3155;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
 (4424, 3155, 0, 0, 0, 0, 0, 0, 6000, 23911);
 
 -- 201091 Mortal Wounds
-DELETE FROM `spell_script_names` WHERE `entry`=201091;
+DELETE FROM `spell_script_names` WHERE `spell_id`=201091;
 INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(201091,"spell_hun_mortal_wounds");
 
 -- 206505 Murder of Crows (Survival)
-DELETE FROM `spell_script_names` WHERE `entry`=206505;
+DELETE FROM `spell_script_names` WHERE `spell_id`=206505;
 INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(206505,"spell_hun_murder_crows");
 
 -- 87935 Serpent Sting
@@ -75,7 +75,7 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (186270,'spell_hun_raptor_strike');   -- 186270 Raptor strike
 
 -- 201078 Snake Hunter
-DELETE FROM `spell_script_names` WHERE `entry`=201078;
+DELETE FROM `spell_script_names` WHERE `spell_id`=201078;
 INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(201078,"spell_hun_snake_hunter");
 
 -- 162496 Steel Trap
@@ -83,12 +83,12 @@ DELETE FROM `areatrigger_template` WHERE `Id`=3161;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3161, 0, 0, 3, 3, 0, 0, 0, 0, "areatrigger_hun_steel_trap", 23911);
 
-DELETE FROM `spell_areatrigger` WHERE `SpellMiscId`=3160;
+DELETE FROM `spell_areatrigger` WHERE `AreaTriggerId`=3161;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
-(2392, 3160, 0, 0, 0, 0, 0, 0, 60000, 23911);
+(2392, 3161, 0, 0, 0, 0, 0, 0, 60000, 23911);
 
 -- 191241 Sticky Bomb
-DELETE FROM `spell_script_names` WHERE `entry`=191241;
+DELETE FROM `spell_script_names` WHERE `spell_id`=191241;
 INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(191241,"spell_hun_sticky_bomb");
 
 -- 187699 Tar trap starter areatrigger
@@ -96,7 +96,7 @@ DELETE FROM `areatrigger_template` WHERE `Id`=3159;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3159, 0, 0, 5, 5, 0, 0, 0, 0, "areatrigger_hun_tar_trap", 23911);
 
-DELETE FROM `spell_areatrigger` WHERE `SpellMiscId`=3159;
+DELETE FROM `spell_areatrigger` WHERE `AreaTriggerId`=3159;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
 (4435, 3159, 0, 0, 0, 0, 0, 0, 60000, 23911);
 
@@ -105,6 +105,6 @@ DELETE FROM `areatrigger_template` WHERE `Id`=3160;
 INSERT INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES
 (3160, 0, 0, 5, 5, 0, 0, 0, 0, "areatrigger_hun_tar_trap_pool", 23911);
 
-DELETE FROM `spell_areatrigger` WHERE `SpellMiscId`=3160;
+DELETE FROM `spell_areatrigger` WHERE `AreaTriggerId`=3160;
 INSERT INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES
 (4436, 3160, 0, 0, 0, 0, 0, 0, 30000, 23911);
