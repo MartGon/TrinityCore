2a4cb5fef7e956f93daab4d541f36c23e467362c
MartGon <gudo35@gmail.com>
2017-06-21 23:52:31 +0200

    Fixes for master of subtlety

--- sql/updates/world/master/2017_99_99_96_world.sql
+++ sql/updates/world/master/2017_99_99_96_world.sql
@@ -2,3 +2,7 @@
  -- 185313 - Shadow Dance
 DELETE FROM `spell_script_names` WHERE `spell_id`=185313;
 INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(185313,"spell_rog_shadow_dance_aura");
+
+ -- 31223 - Master of subtlety passive
+DELETE FROM `spell_script_names` WHERE `spell_id`=31223;
+INSERT INTO `spell_script_names`(spell_id,ScriptName) VALUES(31223,"spell_rog_master_of_subtlety_passive");
